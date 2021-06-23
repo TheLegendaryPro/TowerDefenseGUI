d9229b6c32fb6040f26a2572455a0b7cbd61e6ac
User <sample@email.com>
2021-06-23 17:30:04 +0800

    changed delay for sending troops

--- autosend.py
+++ autosend.py
@@ -2,18 +2,19 @@ import keyboard
 import pyautogui
 import time
 
-pyautogui.MINIMUM_DURATION = 0.25
+pyautogui.MINIMUM_DURATION = 0.2
+sleep_duration = 0.7
 
 
 def send_troop(troop_xy, send_xy):
         troop_x, troop_y = troop_xy
         pyautogui.rightClick()
-        time.sleep(1)
+        time.sleep(sleep_duration)
         keyboard.press("shift")
         pyautogui.moveTo(troop_x, troop_y)
         pyautogui.click()
         keyboard.release("shift")
-        time.sleep(1)
+        time.sleep(sleep_duration)
         send_x, send_y = send_xy
         pyautogui.moveTo(send_x, send_y)
         pyautogui.click()
